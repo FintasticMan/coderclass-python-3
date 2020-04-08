eb701f16333ffdfd9eb7dd4c410b0b375b867a84
FintasticMan <finlay.davidson@coderclass.nl>
2020-04-08 19:16:06 +0200

    remove global variables, clean up code, and add future plans

--- eindopdracht.py
+++ eindopdracht.py
@@ -1,28 +1,27 @@
 #!/usr/bin/env python3
 
+def menu():
+    print("So, this place is called a 'menu' and this is where you choose what you want to do!\nThe things you can do are:\n Kill a unicorn (k);\n Spare the unicorn (s);\n Be an amazing person (a).\n\n And, if you REALLY want to, you can also quit (q).\n")
+    wotIWantToDo = input(" What do you want to do? (k/s/a/Q) ")
+    print("")
+    return wotIWantToDo
+
 def main():
-    global keepGoing
     keepGoing = True
     print("Welcome to the most awesome-est program ever! (It's so awesome, I don't even know what it does yet!)\n")
     while keepGoing:
-        menu()
-
-def menu():
-    global keepGoing
-    print("So, this place is called a 'menu' and this is where you choose what you want to do!\nSo, the things you can do are:\n\tKill a unicorn (k);\n Spare the unicorn (s);\n Be an amazing person (a).\n\n And, if you REALLY want to, you can also quit (q).\n")
-    wotIWantToDo = input(" What do you want to do? (k/s/a/Q) ")
-    print("")
-    if wotIWantToDo == "k":
-        killUnicorn()
-    elif wotIWantToDo == "s":
-        spareUnicorn()
-    elif wotIWantToDo == "a":
-        beAmazing()
-    elif wotIWantToDo == "q" or wotIWantToDo == "":
-        keepGoing = False
-        print("Bye bye!")
-    else:
-        print("I think you messed up and typed something in wrong. Do better next time!\n")
+        wotIWantToDo = menu()
+        if wotIWantToDo == "k":
+            killUnicorn()
+        elif wotIWantToDo == "s":
+            spareUnicorn()
+        elif wotIWantToDo == "a":
+            beAmazing()
+        elif wotIWantToDo == "q" or wotIWantToDo == "":
+            print("Bye bye!")
+            keepGoing = False
+        else:
+            print("I think you messed up and typed something in wrong. Do better next time!\n")
 
 def killUnicorn():
     print("Killed the unicorn\n")
@@ -34,3 +33,5 @@ def beAmazing():
     print("You're already amazing!\n")
 
 main()
+
+# plans for this project: have 3 functions, a prime checker, a prime lister (that just writes the primes to stdout continuously), and a prime lister (that prints primes between given integers). have 2 files, one that records primes so that on subsequent runs it doesn't have to recalculate, and another one that lists the prime number, how long it took to calculate it (in seconds), and how long is took to calculate all of the primes preceding it
