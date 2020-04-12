c6c2190b8401a5156703376bbddbfa11beb2c835
FintasticMan <finlay.davidson@coderclass.nl>
2020-04-12 11:27:47 +0200

    start work on actual project now it can print primes, but not yet save or load them to a file

--- eindopdracht.py
+++ eindopdracht.py
@@ -1,37 +1,88 @@
 #!/usr/bin/env python3
 
 def menu():
-    print("So, this place is called a 'menu' and this is where you choose what you want to do!\nThe things you can do are:\n Kill a unicorn (k);\n Spare the unicorn (s);\n Be an amazing person (a).\n\n And, if you REALLY want to, you can also quit (q).\n")
-    wotIWantToDo = input(" What do you want to do? (k/s/a/Q) ")
-    print("")
+    print("So, this place is called a 'menu' and this is where you choose what you want to do!\nThe things you can do are:\n Test whether a number is prime [t];\n Continuously list primes until you press q or esc [c];\n List primes in a certain range [l];\n Load previously generated primes from file [f]\n\n And, if you REALLY want to, you can also quit [q].\n")
+    wotIWantToDo = input(" What do you want to do? [t/c/l/f/Q] ")
     return wotIWantToDo
 
 def main():
     keepGoing = True
-    print("Welcome to the most awesome-est program ever! (It's so awesome, I don't even know what it does yet!)\n")
+    print("Welcome to the Prime Mansion! Here you can do a bunch of things with primes! (If you haven't picked it up already, I like prime numbers)\n")
     while keepGoing:
         wotIWantToDo = menu()
-        if wotIWantToDo == "k":
-            killUnicorn()
-        elif wotIWantToDo == "s":
-            spareUnicorn()
-        elif wotIWantToDo == "a":
-            beAmazing()
+        if wotIWantToDo == "t":
+            if primeChecker(int(input("\nWhich positive integer would you like to test? "))):
+                print("\nThat IS a prime!\n")
+            else:
+                print("\nNope, not a prime. Sorry.\n")
+        elif wotIWantToDo == "c":
+            infPrimeLister()
+        elif wotIWantToDo == "l":
+            rangePrimeLister(int(input("\nAt what point would you like the listed primes to start? ")), int(input("And at what point would you like them to end? ")))
+        elif wotIWantToDo == "f":
+            loadFromFile
         elif wotIWantToDo == "q" or wotIWantToDo == "":
-            print("Bye bye!")
+            print("\nBye bye!")
             keepGoing = False
         else:
-            print("I think you messed up and typed something in wrong. Do better next time!\n")
+            print("\nI think you messed up and typed something in wrong. Do better next time!\n")
 
-def killUnicorn():
-    print("Killed the unicorn\n")
+def primeChecker(intPotPrime):
+    isPrime = True
+    i = 2
+    if intPotPrime == 1:
+        isPrime = False
+    else:
+        while isPrime and i <= intPotPrime / 2:
+            if intPotPrime % i == 0:
+                isPrime = False
+            else:
+                i += 1
+    return isPrime
+def infPrimeLister():
+    keepGoing = True
+    listPrimes = []
+    intPotPrime = 2
+    while keepGoing:
+        isPrime = True
+        intIndex = 0
+        i = listPrimes[intIndex]
+        while isPrime and i <= intPotPrime / 2:
+            if intPotPrime % i == 0:
+                isPrime = False
+            else:
+                intIndex += 1
+                i = listPrimes[intIndex]
+        if isPrime:
+            listPrimes.append(intPotPrime)
+            print(intPotPrime)
+        intPotPrime += 1
+    wantToSave = input("\nDo you want to save these primes to a file for future convenience? [Y/n] ")
+    print("\nUnder construction\n")
 
-def spareUnicorn():
-    print("Spared the unicorn\n")
+def rangePrimeLister(intStart, intEnd):
+    listPrimes = [2]
+    for intPotPrime in range(3, intEnd + 1):
+        isPrime = True
+        intIndex = 0
+        i = listPrimes[intIndex]
+        while isPrime and i <= intPotPrime / 2:
+            if intPotPrime % i == 0:
+                isPrime = False
+            else:
+                intIndex += 1
+                i = listPrimes[intIndex]
+        if isPrime:
+            listPrimes.append(intPotPrime)
+    startPrint = 0
+    while listPrimes[startPrint] < intStart:
+        startPrint += 1
+    for i in range(startPrint, len(listPrimes)):
+        print(listPrimes[i])
+    wantToSave = input("\nDo you want to save these primes to a file for future convenience? [Y/n] ")
+    print("\nUnder construction\n")
 
-def beAmazing():
-    print("You're already amazing!\n")
+def loadFromFile:
+    print("\nUnder construction\n")
 
 main()
-
-# plans for this project: have 3 functions, a prime checker, a prime lister (that just writes the primes to stdout continuously), and a prime lister (that prints primes between given integers). have 2 files, one that records primes so that on subsequent runs it doesn't have to recalculate, and another one that lists the prime number, how long it took to calculate it (in seconds), and how long is took to calculate all of the primes preceding it
